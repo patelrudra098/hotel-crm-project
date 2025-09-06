0094a93c3a123d268bec8e6791f35a37b4a585bd
Rudra <reach.rudrapatel@gmail.com>
2025-09-06 19:54:31 +0530

    init express backend

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API running...'));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
